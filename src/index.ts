8d68491d30851f0e4e2784c53f71e89febf120bb
Mastergamer433 <teknikgillaren@gmail.com>
2022-06-08 07:30:52 +0200

    debugging

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,15 @@ const main = async () => {
 clear();
 console.log(
   chalk.red(
-    figlet.textSync('pizza-cli', { horizontalLayout: 'full' })
+    figlet.textSync('SkolmatenCLI', { horizontalLayout: 'full' })
   )
 );
 }
+
+program
+  .version('0.0.1')
+  .description("Ett Command Line Interface för skolmaten.se.")
+  .option('-c, --configure', 'Ställ in alla inställningar att vara rätt.')
+  .parse(process.argv)
+
 
